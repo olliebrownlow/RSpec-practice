e1ba5f4725e0476b168289ae395e7b75279d2973
EC2 Default User <ec2-user@ip-172-31-28-235.us-east-2.compute.internal>
2019-04-13 16:09:04 +0000

    first commit

---
+++ greeter/greeter_spec.rb
@@ -0,0 +1,10 @@
+require './greeter'
+
+describe 'greeter' do
+  it 'greets Rico' do
+    expect(greet("Rico")).to eq "Hello, Rico, how are you today?"
+  end
+  it 'greets Chloe' do
+    expect(greet('Chloe')).to eq 'Hello, Chloe, how are you today?'
+  end
+end
