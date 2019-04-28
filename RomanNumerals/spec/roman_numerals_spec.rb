6af68731569ec29d3a519591a8679648adb25201
EC2 Default User <ec2-user@ip-172-31-28-235.us-east-2.compute.internal>
2019-04-28 12:48:05 +0000

    adds Roman numeral converter

---
+++ RomanNumerals/spec/roman_numerals_spec.rb
@@ -0,0 +1,26 @@
+require 'roman_numerals'
+describe RomanNumerals do
+  it "should be able to convert 1 to I" do
+    expect(RomanNumerals.convert(1)).to eq("I")
+  end
+  
+  it "should be able to convert 5 to V" do
+    expect(RomanNumerals.convert(5)).to eq("V")
+  end
+  
+  it "should be able to convert 10 to X" do
+    expect(RomanNumerals.convert(10)).to eq("X")
+  end
+  
+  it "should be able to convert 35 to XXXV" do
+    expect(RomanNumerals.convert(35)).to eq("XXXV")
+  end
+  
+   it "should be able to convert 996 to CMXCVI" do
+    expect(RomanNumerals.convert(996)).to eq("CMXCVI")
+  end
+  
+  it "should be able to convert 1999 to MCMXCIX" do
+    expect(RomanNumerals.convert(1999)).to eq("MCMXCIX")
+  end
+end
